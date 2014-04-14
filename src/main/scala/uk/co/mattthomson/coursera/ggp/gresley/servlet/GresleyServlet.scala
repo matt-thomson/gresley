b1b9fa4232448fa1eeb9d224791ad9c73c9a8a51
Matt Thomson <matt.thomson@cantab.net>
2014-04-14 19:35:50 +0100

    Add CORS

--- src/main/scala/uk/co/mattthomson/coursera/ggp/gresley/servlet/GresleyServlet.scala
+++ src/main/scala/uk/co/mattthomson/coursera/ggp/gresley/servlet/GresleyServlet.scala
@@ -1,21 +1,25 @@
 package uk.co.mattthomson.coursera.ggp.gresley.servlet
 
-import org.scalatra.{NoContent, AsyncResult, FutureSupport, ScalatraServlet}
+import org.scalatra._
 import org.slf4j.LoggerFactory
-import akka.actor.{Props, ActorSystem}
-import akka.pattern.ask
-import akka.util.Timeout
+import _root_.akka.actor.{Props, ActorSystem}
+import _root_.akka.pattern.ask
+import _root_.akka.util.Timeout
 import scala.concurrent.duration._
 import uk.co.mattthomson.coursera.ggp.gresley.player.GameManager
 import uk.co.mattthomson.coursera.ggp.gresley.gdl.GameProtocolMessage
 
-class GresleyServlet(system: ActorSystem, playerProps: Props) extends ScalatraServlet with FutureSupport {
+class GresleyServlet(system: ActorSystem, playerProps: Props) extends ScalatraServlet with FutureSupport with CorsSupport {
   private val logger = LoggerFactory.getLogger(getClass)
   private val manager = system.actorOf(Props(new GameManager(playerProps)))
 
   implicit val defaultTimeout = Timeout(1.minute)
 
   override protected implicit def executor = system.dispatcher
+
+  options("/*"){
+    response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
+  }
 
   get("/") {
     "Hello, my name is Gresley."
