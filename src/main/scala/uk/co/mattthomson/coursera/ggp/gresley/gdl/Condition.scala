38021bcc906e2bab62938f3ab5e6fef2298a7a7c
Matt Thomson <mattjohnthomson@gmail.com>
2014-04-15 13:28:58 +0100

    Prepare for legal actions

--- src/main/scala/uk/co/mattthomson/coursera/ggp/gresley/gdl/Condition.scala
+++ src/main/scala/uk/co/mattthomson/coursera/ggp/gresley/gdl/Condition.scala
@@ -1,21 +1,22 @@
 package uk.co.mattthomson.coursera.ggp.gresley.gdl
 
 trait Condition {
-  def matches(completeFact: Fact, values: Map[String, String]): Option[Map[String, String]]
+  def matches(completeFacts: Set[Fact], values: Map[String, String]): Set[Map[String, String]]
 }
 
 case class FactCondition(fact: Fact) extends Condition {
-  override def matches(completeFact: Fact, values: Map[String, String]) = fact.matches(completeFact, values)
+  override def matches(completeFacts: Set[Fact], values: Map[String, String]) =
+    completeFacts.flatMap(fact.matches(_, values))
 }
 
 case class StateCondition(fact: Fact) extends Condition {
-  override def matches(completeFact: Fact, values: Map[String, String]) = ???
+  override def matches(completeFacts: Set[Fact], values: Map[String, String]) = ???
 }
 
 case class ActionCondition(role: Role, action: Action) extends Condition {
-  override def matches(completeFact: Fact, values: Map[String, String]) = ???
+  override def matches(completeFacts: Set[Fact], values: Map[String, String]) = ???
 }
 
 case class DistinctCondition(terms: Seq[Term]) extends Condition {
-  override def matches(completeFact: Fact, values: Map[String, String]) = ???
+  override def matches(completeFacts: Set[Fact], values: Map[String, String]) = ???
 }
--- src/main/scala/uk/co/mattthomson/coursera/ggp/gresley/gdl/Conditional.scala
+++ src/main/scala/uk/co/mattthomson/coursera/ggp/gresley/gdl/Conditional.scala
@@ -2,14 +2,10 @@ package uk.co.mattthomson.coursera.ggp.gresley.gdl
 
 case class Conditional(conclusion: Fact, conditions: Seq[Condition]) extends Statement {
   def propagate(facts: Set[Fact]): Set[Fact] = {
-    def matchCondition(values: Set[Map[String, String]], condition: Condition) = (for {
-      f <- facts
-      v <- values
-      m <- condition.matches(f, v)
-    } yield m).toSet
+    def matchCondition(values: Set[Map[String, String]], condition: Condition) = values.flatMap(condition.matches(facts, _))
 
     val values = conditions.foldLeft(Set[Map[String, String]](Map()))(matchCondition)
-    val newFacts = values.map(conclusion.substitute)
+    val newFacts = values.toSet.map(conclusion.substitute)
     (facts ++ newFacts).toSet
   }
 }
