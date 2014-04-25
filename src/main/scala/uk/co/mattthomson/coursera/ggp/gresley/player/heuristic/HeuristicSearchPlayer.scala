bef96997124c17910067686af0db0b35f37f43aa
Matt Thomson <mattjohnthomson@gmail.com>
2014-04-25 15:54:30 +0100

    Add mobility heuristic player

---
+++ src/main/scala/uk/co/mattthomson/coursera/ggp/gresley/player/heuristic/HeuristicSearchPlayer.scala
@@ -0,0 +1,69 @@
+package uk.co.mattthomson.coursera.ggp.gresley.player.heuristic
+
+import uk.co.mattthomson.coursera.ggp.gresley.player.Player
+import uk.co.mattthomson.coursera.ggp.gresley.gdl.{Action, GameState, GameDescription}
+import akka.actor.ActorRef
+
+abstract class HeuristicSearchPlayer(depthLimit: Int) extends Player[Seq[String]] {
+  override def initialize(game: GameDescription, role: String): Seq[String] = game.roles.filter(_ != role)
+
+  override def play(state: GameState, role: String, source: ActorRef, otherRoles: Seq[String]): Seq[String] = {
+    val chosenAction = bestMove(state, role, otherRoles)
+    log.info(s"Chosen action: $chosenAction")
+
+    source ! chosenAction
+    otherRoles
+  }
+
+  private def bestMove(state: GameState, role: String, otherRoles: Seq[String]) = {
+    val legalActions = state.legalActions(role)
+    if (legalActions.size == 1) legalActions.head else {
+      val initialAction: Option[Action] = None
+      val (_, bestAction) = legalActions.foldLeft((-1, initialAction))(tryNextMinScore(state, role, otherRoles, 101, 1))
+      bestAction.get
+    }
+  }
+
+  private def minScore(state: GameState, role: String, otherRoles: Seq[String], alpha: Int, beta: Int, level: Int)(action: Action): Int = {
+    val otherActions: Seq[Map[String, Action]] = otherRoles.foldLeft(Seq(Map[String, Action]()))(addAction(state))
+
+    otherActions.foldLeft(beta)(tryNextMaxScore(state, action, role, otherRoles, alpha, level + 1))
+  }
+
+  private def maxScore(role: String, otherRoles: Seq[String], level: Int)(state: GameState, alpha: Int, beta: Int): Int = {
+    if (state.isTerminal) state.value(role)
+    else if (level > depthLimit) intermediateStateValue(state, role)
+    else {
+      val initialAction: Option[Action] = None
+      val (bestScore, _) = state.legalActions(role).foldLeft((alpha, initialAction))(tryNextMinScore(state, role, otherRoles, beta, level))
+      bestScore
+    }
+  }
+
+  private def addAction(state: GameState)(soFar: Seq[Map[String, Action]], role: String): Seq[Map[String, Action]] = {
+    soFar.flatMap(actions => state.legalActions(role).map { action => actions + (role -> action) })
+  }
+
+  private def tryNextMinScore(state: GameState, role: String, otherRoles: Seq[String], beta: Int, level: Int)(bestSoFar: (Int, Option[Action]), action: Action) = {
+    val (alpha, _) = bestSoFar
+    if (alpha == 100) bestSoFar
+    else if (alpha >= beta) (beta, None)
+    else {
+      val score = minScore(state, role, otherRoles, alpha, beta, level)(action)
+      if (score > alpha) (score, Some(action)) else bestSoFar
+    }
+  }
+
+  private def tryNextMaxScore(state: GameState, action: Action, role: String, otherRoles: Seq[String], alpha: Int, level: Int)(beta: Int, otherActions: Map[String, Action]): Int = {
+    if (beta == 0) beta
+    else if (beta <= alpha) alpha
+    else {
+      val actions = Map(role -> action) ++ otherActions
+      val newState = state.update(actions)
+      val score = maxScore(role, otherRoles, level)(newState, alpha, beta)
+      if (score < beta) score else beta
+    }
+  }
+
+  protected def intermediateStateValue(state: GameState, role: String): Int
+}
--- src/test/scala/uk/co/mattthomson/coursera/ggp/gresley/player/heuristic/BoundedDepthSearchPlayerSpec.scala
+++ src/test/scala/uk/co/mattthomson/coursera/ggp/gresley/player/heuristic/BoundedDepthSearchPlayerSpec.scala
@@ -2,11 +2,10 @@ package uk.co.mattthomson.coursera.ggp.gresley.player.heuristic
 
 import uk.co.mattthomson.coursera.ggp.gresley.player.{PlayerSpec, LegalPlayer}
 import akka.actor.Props
-import uk.co.mattthomson.coursera.ggp.gresley.player.multiple.AlphaBetaPlayer
 
 class BoundedDepthSearchPlayerSpec extends PlayerSpec {
   "The bounded depth search player" should "play Hunter" in {
-    val player = system.actorOf(Props(new BoundedDepthSearchPlayer(2)))
+    val player = system.actorOf(Props(new BoundedDepthSearchPlayer(3)))
 
     val finalState = playGame("hunter", List(player))
 
