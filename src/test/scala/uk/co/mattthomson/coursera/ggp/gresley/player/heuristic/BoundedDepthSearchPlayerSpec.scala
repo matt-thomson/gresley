dfae0c0a5119df2a31da9771a9f9b9f66360b2ce
Matt Thomson <mattjohnthomson@gmail.com>
2014-04-25 15:06:37 +0100

    Start with bounded depth search player

---
+++ src/test/scala/uk/co/mattthomson/coursera/ggp/gresley/player/heuristic/BoundedDepthSearchPlayerSpec.scala
@@ -0,0 +1,24 @@
+package uk.co.mattthomson.coursera.ggp.gresley.player.heuristic
+
+import uk.co.mattthomson.coursera.ggp.gresley.player.{PlayerSpec, LegalPlayer}
+import akka.actor.Props
+import uk.co.mattthomson.coursera.ggp.gresley.player.multiple.AlphaBetaPlayer
+
+class BoundedDepthSearchPlayerSpec extends PlayerSpec {
+  "The bounded depth search player" should "play Hunter" in {
+    val player = system.actorOf(Props(new BoundedDepthSearchPlayer(2)))
+
+    val finalState = playGame("hunter", List(player))
+
+    finalState.value("robot") should be (22)
+  }
+
+  it should "play Alquerque" ignore {
+    val player = system.actorOf(Props(new BoundedDepthSearchPlayer(2)))
+    val opponent = system.actorOf(Props[LegalPlayer])
+
+    val finalState = playGame("alquerque", List(player, opponent))
+
+    finalState.value("red") should be (100)
+  }
+}
