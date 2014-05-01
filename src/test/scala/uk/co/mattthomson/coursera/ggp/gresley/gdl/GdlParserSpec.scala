2f90d8f1c5932b25cb23049025e42946b74ce0bf
Matt Thomson <mattjohnthomson@gmail.com>
2014-05-01 13:44:51 +0100

    Add test for game that fails to parse

--- src/test/scala/uk/co/mattthomson/coursera/ggp/gresley/gdl/GdlParserSpec.scala
+++ src/test/scala/uk/co/mattthomson/coursera/ggp/gresley/gdl/GdlParserSpec.scala
@@ -174,6 +174,11 @@ class GdlParserSpec extends FlatSpec with ShouldMatchers {
     GameDescription(game)
   }
 
+  it should "parse English Draughts" in {
+    val game = Source.fromFile("src/test/resources/games/englishDraughts.kif").mkString
+    GameDescription(game)
+  }
+
   private def parse(input: String, expected: Any*) {
     val result = gdlParser.parseAll(gdlParser.game, input)
     if (result.successful) result.get should be(expected) else fail(result.toString)
