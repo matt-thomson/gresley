70c5aee9c65e180ceea07d03d6aa20f9dba2deeb
Matt Thomson <mattjohnthomson@gmail.com>
2014-05-02 12:18:05 +0100

    Sort out parsing

--- src/main/scala/uk/co/mattthomson/coursera/ggp/gresley/gdl/GdlParser.scala
+++ src/main/scala/uk/co/mattthomson/coursera/ggp/gresley/gdl/GdlParser.scala
@@ -24,9 +24,9 @@ class GdlParser extends RegexParsers {
     "not\\b".r |
     "or\\b".r
 
-  private def name: Parser[String] = not(reserved) ~> "[a-zA-Z0-9_]+".r
+  private def name: Parser[String] = not(reserved) ~> "[a-zA-Z0-9]+".r
   private def variableName: Parser[String] = "[a-zA-Z0-9]+".r
-  private def id: Parser[String] = """[a-zA-Z0-9\.\-]+""".r
+  private def id: Parser[String] = """[a-zA-Z0-9\.\-\_]+""".r
 
   private def term = variableTerm | literalTerm
   private def variableTerm = "?" ~> variableName ^^ { name => VariableTerm(name) }
